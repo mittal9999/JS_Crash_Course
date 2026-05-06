081ecc653effad2f6977471f2e8432bda908b574
mittal9999 <mittalrathva99@gmail.com>
2026-05-06 11:57:56 +0530

    added Js basic concept

---
+++ main.js
@@ -0,0 +1,58 @@
+// alert('Hello world');
+console.log('Hello world');
+console.error('This is an error');
+console.warn('This is warning');
+
+// variables  var , let , const
+// let age = 30;
+// const age = 30;
+// age =31;
+
+// console.log(age);
+
+// let score;
+// const score = 10;
+// // score = 10;
+// console.log(score);
+
+
+// DATA TYPES strings , numbers , boolean , null ,undefined 
+
+// const name ='mittal';
+// const age= 27;
+// const rating = 4.5;
+// const isCool =  true;
+// const x =null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof z);
+
+
+
+// const name = 'abc';
+// const age = 25;
+
+// // Concatenation
+// console.log('My name is '  + name +  ' and I am  ' + age);
+
+// Teamplate string
+
+// console.log(`My name is ${name} and I am ${age}`);
+
+// const s = 'Hello World';
+
+// console.log(s.length);
+
+// Arrays- variables that hold mmultiple values
+// const numbers = new Array(1,2,3,4,5);
+// const fruits = ['apples', 'orange', 'peard', 10, true];
+
+// fruits[3] = 'grapes';
+// fruits.push('mangos');
+// fruits.unshift('strawberries');
+// fruits.pop();
+// console.log(fruits.indexOf('orange'));
+// console.log(fruits);
+
+
