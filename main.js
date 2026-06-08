360a05579619e60ae851c982a35dc80c84727dd6
mittal9999 <mittalrathva99@gmail.com>
2026-06-08 22:47:21 +0530

    Added all topic

--- main.js
+++ main.js
@@ -262,7 +262,48 @@ switch (color) {
 // window.alert(1);
 
 //  Single element 
- const form = (document.getElementById('my-form')); 
-  console.log(document.querySelector('h1'));
+//  const form = (document.getElementById('my-form')); 
+//   console.log(document.querySelector('h1'));
 
-//   
+//   Multiple element
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+
+const ul = document.querySelector('.items');
+// console.log(ul);
+//  ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h4>HELLO</h4>';
+
+
+const btn = document.querySelector('.btn');
+
+// btn.style.background = 'red';
+btn.addEventListener('mouseout', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form')
+        .style.background = '#ccc';
+
+});
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput= document.querySelector('#email');
+const msg= document.querySelector('.msg');
+const userList = document.querySelector('#users');
+myForm.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+    e.preventDefault();
+   if(nameInput.value === '' || emailInput.value ==='') {
+    msg.innerHTML= 'Please enter All fields';
+   } else{
+    console.log('success');
+   }
+} 
